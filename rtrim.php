db6ad3ede5d983025f0965271d95c0ff0cd52739
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-10-18 11:21:13 -0400

    tried the trim function

---
+++ rtrim.php
@@ -0,0 +1,7 @@
+<?php
+	$str="Hi my name is Bikram and I go to NJIT";
+	$newStr = rtrim($str,'Bikram');
+	echo $str."<br>";
+	//TRIED TO PRINT BOTH TO SEE THE DIFFERENCE BUT THIS IS NOT WORKING
+	echo $newStr;
+?>
